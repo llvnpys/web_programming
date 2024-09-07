75df5c339d619fcdcc32aa66a226f1e29588c3e2
llvnpys <naysung03@hanyang.ac.kr>
2024-09-08 00:35:57 +0900

    bulletin board CRUD has been completed

--- spring-jpa/src/main/java/com/example/springjpa/controller/BoardController.java
+++ spring-jpa/src/main/java/com/example/springjpa/controller/BoardController.java
@@ -1,13 +1,20 @@
 package com.example.springjpa.controller;
 
+import com.example.springjpa.domain.Board;
+import com.example.springjpa.dto.BoardDTO;
 import com.example.springjpa.dto.PageRequestDTO;
+import com.example.springjpa.dto.PageResponseDTO;
 import com.example.springjpa.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -19,6 +26,46 @@ public class BoardController {
 
     @GetMapping("list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
+        model.addAttribute("responseDTO", responseDTO);
+    }
+
+    @GetMapping("/register")
+    public void registerGet() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult
+            , RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+        boardService.remove(bno);
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/list";
     }
 }
--- spring-jpa/src/main/java/com/example/springjpa/repository/BoardRepository.java
+++ spring-jpa/src/main/java/com/example/springjpa/repository/BoardRepository.java
@@ -5,7 +5,7 @@ import com.example.springjpa.repository.search.BoardSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+public interface  BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
 
     @Query(value = "select now()", nativeQuery = true)
     String getTime();
